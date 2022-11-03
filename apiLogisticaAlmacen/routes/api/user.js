bd2198b817ad2e3dfa4ef47cd0a81ec4064a6eda
Santander Lincoln <ravicla1993@gmail.com>
2022-11-03 14:43:42 -0500

    ajuste de error

--- apiLogisticaAlmacen/routes/api/user.js
+++ apiLogisticaAlmacen/routes/api/user.js
@@ -1,15 +1,17 @@
 const { getAll } = require('../../models/user.model');
 
-const router = require('express').Router;
+const router = require('express').Router();
 
 
 router.get('/', (req, res) => {
-    getAll()
-        .then(user => {
-            res.json(user);
-        })
-        .catch((error) => {
-            res.json({ fatal: error.message });
-        }); 
+  getAll()
+      .then(user => {
+          res.json(user);
+      })
+      .catch((error) => {
+          res.json({ fatal: error.message });
+      }); 
 })
 
+
+module.exports = router;
