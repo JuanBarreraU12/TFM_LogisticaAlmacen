ee87890519aaedcd55e5415a4189e99ace50f54a
Esteban Marín <estebanamm31@gmail.com>
2022-11-07 16:42:54 -0600

    Se elimina proyecto anterior para crearlo sin vistas y apuntando a las nuevas tablas de BD

--- apiLogisticaAlmacen/models/rol.model.js
+++
@@ -1,9 +0,0 @@
-const { executeQuery, executeQueryOne } = require('../helpers/utils');
-
-const getRolById = (rolId) => {
-    return executeQueryOne('select * from rol where id = ?', [rolId]);
-}
-
-module.exports = {
-  getRolById
-}
--- apiLogisticaAlmacen/routes/api.js
+++
@@ -1,6 +0,0 @@
-const router = require('express').Router();
-
-router.use('/employee', require('./api/employee'));
-router.use('/users', require('./api/users'));
-
-module.exports = router;
