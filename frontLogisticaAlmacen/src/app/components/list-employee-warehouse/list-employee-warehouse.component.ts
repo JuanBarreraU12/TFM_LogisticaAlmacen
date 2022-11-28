d8f42e2e9bb5aa10cc21ca210125f92c1ddb167b
Thalia <ravicla1993@gmail.com>
2022-11-27 23:02:24 -0500

    lista de almacenes de empleado

--- frontLogisticaAlmacen/src/app/components/list-employee-warehouse/list-employee-warehouse.component.ts
+++ frontLogisticaAlmacen/src/app/components/list-employee-warehouse/list-employee-warehouse.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { Warehouse } from 'src/app/interfaces/warehouse.interface';
 import { WarehouseService } from 'src/app/services/warehouse.service';
 
@@ -11,18 +12,15 @@ export class ListEmployeeWarehouseComponent implements OnInit {
 
   arrWarehouse: Warehouse[] = []
   constructor(
-    private warehouseServices: WarehouseService
+    private warehouseServices: WarehouseService,
+    private activateRoute: ActivatedRoute
   ) { }
 
   ngOnInit(): void {
-    this.getWarehouse
+    this.activateRoute.params.subscribe(async (params: any) => {
+      let id: number = parseInt(params.idemployee);
+      let response = await this.warehouseServices.getWarehousebyIdEmployee(id);
+      this.arrWarehouse = response;
+    } )
   }
-  
-  async getWarehouse(): Promise<void>{
-    try {
-      let response = await this.warehouseServices.getAllWarehouse()
-      this.arrWarehouse = response
-    } catch(err) { }
-  }
-
 }
