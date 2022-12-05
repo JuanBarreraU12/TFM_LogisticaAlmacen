af2556653bde802962f2d6d9f1a166716758a325
Esteban Marín <estebanamm31@gmail.com>
2022-12-05 14:07:42 -0600

    Se sube script del respaldo de la BD

---
+++ db/gestionAlmacen_05122022.sql
@@ -0,0 +1,343 @@
+-- MySQL dump 10.13  Distrib 8.0.30, for Win64 (x86_64)
+--
+-- Host: localhost    Database: gestion_almacen
+-- ------------------------------------------------------
+-- Server version	8.0.30
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `locations`
+--
+
+DROP TABLE IF EXISTS `locations`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `locations` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `description` varchar(45) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `warehouses_id` int NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_locations_warehouses1` (`warehouses_id`),
+  CONSTRAINT `fk_locations_warehouses1` FOREIGN KEY (`warehouses_id`) REFERENCES `warehouses` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `locations`
+--
+
+LOCK TABLES `locations` WRITE;
+/*!40000 ALTER TABLE `locations` DISABLE KEYS */;
+INSERT INTO `locations` VALUES (1,'Localización 1 ',1),(2,'Localización 2 ',1),(3,'Localización 3',1),(4,'Localización 4 ',2),(5,'Localización 5',2),(6,'Localización 6',2);
+/*!40000 ALTER TABLE `locations` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `material_types`
+--
+
+DROP TABLE IF EXISTS `material_types`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `material_types` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) COLLATE utf8mb4_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `name_UNIQUE` (`name`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `material_types`
+--
+
+LOCK TABLES `material_types` WRITE;
+/*!40000 ALTER TABLE `material_types` DISABLE KEYS */;
+INSERT INTO `material_types` VALUES (1,'Materia Prima'),(4,'Mercancía'),(3,'Producto Terminado'),(2,'Suministro de Fábrica');
+/*!40000 ALTER TABLE `material_types` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `materials`
+--
+
+DROP TABLE IF EXISTS `materials`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `materials` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `price` decimal(18,2) unsigned NOT NULL,
+  `sku` varchar(10) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `material_types_id` int NOT NULL,
+  `image` varchar(256) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `sku_UNIQUE` (`sku`),
+  KEY `fk_materials_material_types1` (`material_types_id`),
+  CONSTRAINT `fk_materials_material_types1` FOREIGN KEY (`material_types_id`) REFERENCES `material_types` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `materials`
+--
+
+LOCK TABLES `materials` WRITE;
+/*!40000 ALTER TABLE `materials` DISABLE KEYS */;
+INSERT INTO `materials` VALUES (2,'Cemento',25.00,'MP-0000001',3,'https://www.rubi.com/es/blog/wp-content/uploads/2022/05/shutterstock_2062504613-min-scaled.jpg'),(3,'Martillo',4.50,'PT-0000001',1,'https://sebastiansalazar.com/wp-content/uploads/2021/05/Martillo-de-carpintero-marca-Bellota-modelo-8007.jpg'),(9,'Broca',4.56,'PT-0000002',1,'https://promart.vteximg.com.br/arquivos/ids/5721932-1000-1000/1000201147.jpg?v=637872912757700000'),(11,'Papel',10.50,'MP-0000002',3,'https://tecnographic.net/wp-content/uploads/2018/11/gramajes-de-papel-2400x1200.jpg');
+/*!40000 ALTER TABLE `materials` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `materials_locations`
+--
+
+DROP TABLE IF EXISTS `materials_locations`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `materials_locations` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `materials_id` int NOT NULL,
+  `locations_id` int NOT NULL,
+  `stock` int unsigned NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `materials_locations_unique` (`materials_id`,`locations_id`),
+  KEY `fk_materials_has_locations_locations1` (`locations_id`),
+  CONSTRAINT `fk_materials_has_locations_locations1` FOREIGN KEY (`locations_id`) REFERENCES `locations` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `fk_materials_has_locations_materials1` FOREIGN KEY (`materials_id`) REFERENCES `materials` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `materials_locations`
+--
+
+LOCK TABLES `materials_locations` WRITE;
+/*!40000 ALTER TABLE `materials_locations` DISABLE KEYS */;
+INSERT INTO `materials_locations` VALUES (1,2,1,45),(5,9,1,50),(6,2,3,12),(7,9,5,25),(9,11,6,125),(10,11,1,325),(12,11,5,150);
+/*!40000 ALTER TABLE `materials_locations` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `order_states`
+--
+
+DROP TABLE IF EXISTS `order_states`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `order_states` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `order_states`
+--
+
+LOCK TABLES `order_states` WRITE;
+/*!40000 ALTER TABLE `order_states` DISABLE KEYS */;
+INSERT INTO `order_states` VALUES (1,'Ingresado'),(2,'Para Revisión'),(3,'Devuelto'),(4,'Para Entrega'),(5,'Enviado'),(6,'Recibido');
+/*!40000 ALTER TABLE `order_states` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `orders`
+--
+
+DROP TABLE IF EXISTS `orders`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `orders` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `out_date` date NOT NULL,
+  `truck_plate` varchar(10) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `comment` text COLLATE utf8mb4_unicode_ci,
+  `order_states_id` int NOT NULL,
+  `warehouses_id_origin` int DEFAULT NULL,
+  `warehouses_id_destiny` int DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_orders_order_states1` (`order_states_id`),
+  KEY `fk_orders_warehouses1` (`warehouses_id_destiny`),
+  KEY `fk_orders_warehouses2` (`warehouses_id_origin`),
+  CONSTRAINT `fk_orders_order_states1` FOREIGN KEY (`order_states_id`) REFERENCES `order_states` (`id`),
+  CONSTRAINT `fk_orders_warehouses1` FOREIGN KEY (`warehouses_id_destiny`) REFERENCES `warehouses` (`id`) ON DELETE SET NULL ON UPDATE SET NULL,
+  CONSTRAINT `fk_orders_warehouses2` FOREIGN KEY (`warehouses_id_origin`) REFERENCES `warehouses` (`id`) ON DELETE SET NULL ON UPDATE SET NULL
+) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `orders`
+--
+
+LOCK TABLES `orders` WRITE;
+/*!40000 ALTER TABLE `orders` DISABLE KEYS */;
+INSERT INTO `orders` VALUES (2,'2022-11-29','HGF-41698','Pedido actualizado por compra de nuevos materiales. ',1,1,2),(3,'2022-05-25','VGF-585',NULL,2,2,NULL),(4,'2022-03-05','BLG-412',NULL,5,1,2),(14,'2022-11-16','LKH-7854','Nuevo pedido ingresado',1,1,2),(16,'2022-11-08','LKH-7854','Prueba de pedido',1,1,2),(17,'2022-11-29','HGF-4169','Pedido de prueba prueba',1,1,2);
+/*!40000 ALTER TABLE `orders` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `orders_details`
+--
+
+DROP TABLE IF EXISTS `orders_details`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `orders_details` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `materials_locations_id` int NOT NULL,
+  `orders_id` int NOT NULL,
+  `quantity` int unsigned NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `orders_details_unique` (`materials_locations_id`,`orders_id`),
+  KEY `fk_materials_locations_has_orders_orders1` (`orders_id`),
+  CONSTRAINT `fk_materials_locations_has_orders_materials_locations1` FOREIGN KEY (`materials_locations_id`) REFERENCES `materials_locations` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `fk_materials_locations_has_orders_orders1` FOREIGN KEY (`orders_id`) REFERENCES `orders` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=210 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `orders_details`
+--
+
+LOCK TABLES `orders_details` WRITE;
+/*!40000 ALTER TABLE `orders_details` DISABLE KEYS */;
+INSERT INTO `orders_details` VALUES (78,9,3,40),(114,1,4,10),(116,10,4,4),(144,5,14,4),(148,12,3,15),(167,5,16,25),(198,1,2,12),(207,10,2,15),(208,5,2,10),(209,10,14,15);
+/*!40000 ALTER TABLE `orders_details` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `roles`
+--
+
+DROP TABLE IF EXISTS `roles`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `roles` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `roles`
+--
+
+LOCK TABLES `roles` WRITE;
+/*!40000 ALTER TABLE `roles` DISABLE KEYS */;
+INSERT INTO `roles` VALUES (1,'Operario'),(2,'Encargado'),(3,'Jefe');
+/*!40000 ALTER TABLE `roles` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `users` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `first_last_name` varchar(45) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `second_last_name` varchar(45) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `dni` varchar(15) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `phone` varchar(45) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `birth_date` varchar(45) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `email` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `password` varchar(250) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `roles_id` int NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `email_UNIQUE` (`email`),
+  UNIQUE KEY `dni_UNIQUE` (`dni`),
+  KEY `fk_users_roles1` (`roles_id`),
+  CONSTRAINT `fk_users_roles1` FOREIGN KEY (`roles_id`) REFERENCES `roles` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=20 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (14,'Esteban','Marín',NULL,'116380004',NULL,NULL,'admin@logistica.com','$2a$08$4O9SYccZklKs5IoNjdthkeNzJc69KznhdoKet.yKXQ/Ck6VI/.EFO',3),(15,'Thalia','Alcivar',NULL,'22464804',NULL,'1985-10-05','operario@logistica.com','$2a$08$7wIhikOXVgsp05lOzskrn.CU3pnmXb3zBuakmMEiwmrpG1U3opvLa',1),(17,'Juan','Barrera',NULL,'115380540',NULL,NULL,'encargado@logistica.com','$2a$08$.TGpZS26xUkTrv9xP4OssOUalomK.TEcDDsu461URR/bYAAYRbh.i',2);
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `users_warehouses`
+--
+
+DROP TABLE IF EXISTS `users_warehouses`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `users_warehouses` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `users_id` int NOT NULL,
+  `warehouses_id` int NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `users_warehouses_unique` (`users_id`,`warehouses_id`),
+  KEY `fk_users_has_warehouses_warehouses1` (`warehouses_id`),
+  CONSTRAINT `fk_users_has_warehouses_users1` FOREIGN KEY (`users_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `fk_users_has_warehouses_warehouses1` FOREIGN KEY (`warehouses_id`) REFERENCES `warehouses` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=28 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users_warehouses`
+--
+
+LOCK TABLES `users_warehouses` WRITE;
+/*!40000 ALTER TABLE `users_warehouses` DISABLE KEYS */;
+INSERT INTO `users_warehouses` VALUES (14,15,1),(17,17,1);
+/*!40000 ALTER TABLE `users_warehouses` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `warehouses`
+--
+
+DROP TABLE IF EXISTS `warehouses`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `warehouses` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `description` varchar(45) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `address` varchar(150) COLLATE utf8mb4_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `warehouses`
+--
+
+LOCK TABLES `warehouses` WRITE;
+/*!40000 ALTER TABLE `warehouses` DISABLE KEYS */;
+INSERT INTO `warehouses` VALUES (1,'Almacén 1','San José, Curridabat, Granadilla Norte'),(2,'Almacén 2','Bogotá, Bogotá');
+/*!40000 ALTER TABLE `warehouses` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-12-05 14:03:51
